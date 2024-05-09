25a0ea3c1d8a99f3f4a6bc7d5871d7343f59a5e1
Mohit Rajbhandari <rbmohit1283@gmail.com>
2024-05-10 03:19:52 +0545

    daily challenge done

--- projectbluesky/lib/forum/forum.dart
+++ projectbluesky/lib/forum/forum.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:projectbluesky/services/challengeServices.dart';
+import 'package:projectbluesky/modal/dailyChallenge.dart';
 import 'package:projectbluesky/signIn/firebaseSignin.dart';
 
 class Forum extends StatefulWidget {
-  const Forum({super.key});
+  const Forum({Key? key});
 
   @override
   State<Forum> createState() => _ForumState();
 }
 
 class _ForumState extends State<Forum> {
-  Firebase _firebase = new Firebase();
+  Firebase _firebase = Firebase();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:  SafeArea(child: ElevatedButton(onPressed: (){
-      _firebase.logout();
-      Navigator.pushNamed(context, '/');
-    }, child: Text('logout'))));
+    return Scaffold(
+      body: SafeArea(
+        child: FutureBuilder<Map<String, DailyChallenge>>(
+          future: getFromFireStore(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasData) {
+              // Extract challenges and their users from snapshot data
+              final challenges = snapshot.data!.values.toList();
+              final users = snapshot.data!.keys.toList();
+
+              return ListView.builder(
+                itemCount: challenges.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('User: ${users[index]}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Title: ${challenges[index].title}'),
+                        Text('Description: ${challenges[index].description}'),
+                        // Add more fields from DailyChallenge as needed
+                        SizedBox(
+                            height: 8), // Add some space between text and image
+                        if (challenges[index].picture != null)
+                          Image.network(
+                            challenges[index].picture!,
+                            width: double
+                                .infinity, // Make the image fill the width of the ListTile
+                            fit: BoxFit
+                                .cover, // Adjust the image to cover the space
+                          ),
+                      ],
+                    ),
+                    // Add onTap handler if needed
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Text('No data available');
+            }
+          },
+        ),
+      ),
+    );
   }
 }
--- projectbluesky/lib/services/challengeServices.dart
+++ projectbluesky/lib/services/challengeServices.dart
@@ -1,13 +1,58 @@
-
-
- import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:projectbluesky/modal/dailyChallenge.dart';
 
 Future<void> addToFirestore(DailyChallenge challenge) async {
-    try {
-      await FirebaseFirestore.instance.collection('daily_challenges').add(challenge.toJson());
-      print('Daily challenge added to Firestore successfully');
-    } catch (error) {
-      print('Error adding daily challenge to Firestore: $error');
-    }
+  try {
+    await FirebaseFirestore.instance
+        .collection('daily_challenges')
+        .add(challenge.toJson());
+    print('Daily challenge added to Firestore successfully');
+  } catch (error) {
+    print('Error adding daily challenge to Firestore: $error');
   }
+}
+
+Future<Map<String, DailyChallenge>> getFromFireStore() async {
+  try {
+    // Get daily challenges
+    var challengeQuerySnapshot =
+        await FirebaseFirestore.instance.collection('daily_challenges').get();
+
+    // Map to store user names and daily challenges
+    Map<String, DailyChallenge> challengeMap = {};
+
+    // Iterate through each challenge document
+    for (var challengeDoc in challengeQuerySnapshot.docs) {
+      // Get user ID from the challenge document
+      var userId = challengeDoc['userID'];
+
+      // Get user document from the users collection
+      var userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .get();
+
+      // Get user name from the user document
+      var userName = userDoc['name'];
+
+      // Create DailyChallenge object
+      var challenge = DailyChallenge(
+        title: challengeDoc['title'],
+        description: challengeDoc['description'],
+        picture: challengeDoc['picture'],
+        userID: challengeDoc['userID'],
+        challengeDate: DateTime.parse(challengeDoc['challengeDate']),
+      );
+
+      // Add entry to challenge map with user name as key and challenge object as value
+      challengeMap[userName] = challenge;
+    }
+
+    return challengeMap;
+  } catch (e) {
+    // Handle errors
+    print('Error fetching challenges: $e');
+    return {}; // Return empty map in case of error
+  }
+}

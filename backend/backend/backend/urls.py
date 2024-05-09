5e1c7c44c97f7e1c5f3086374e5106a17d3eac62
aavashlamichhane <aavashlamichhane@gmail.com>
2024-05-09 17:03:32 +0545

    make migrations and added test functions

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from airQuality.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blueSky/', include('airQuality.urls')),
+    # path('blueSky/', include('airQuality.urls')),
+    path('test/',model_pred)
 ]
